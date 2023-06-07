24cbb106b325856c0185b029fc1be507362541f8
henriqueAmbrosi <henriqueambrosi1256@gmail.com>
2023-06-07 00:04:12 -0300

    correção boss

--- Assets/Scripts/BossScript.cs
+++ Assets/Scripts/BossScript.cs
@@ -7,29 +7,21 @@ public class BossScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed = 0;
-    public GameObject edgeCheck;
     public GameObject bear;
 
-    RaycastHit2D groundInfo;
-    RaycastHit2D playerInfo;
-    float rayDistance = 1;
-    float bearVisionArea = 12.0f;
     public int hp;
     public GameObject enemyDeathFeedback;
     BoxCollider2D[] colliders2D;
-    SpriteRenderer spriteRenderer;
     Animator animator;
     public static BossScript boss;
     bool bossEnabled = false;
-
-    float enragedTime = 0;
+    public bool enraged = false;
 
 
     private void Start()
     {
         boss = this;
         colliders2D = GetComponents<BoxCollider2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
     }
 
@@ -39,21 +31,7 @@ public class BossScript : MonoBehaviour
         if (bossEnabled)
         {
             transform.Translate(Vector2.left * speed * Time.deltaTime);
-
             
-
-            groundInfo = Physics2D.Raycast(edgeCheck.transform.position, Vector2.down, rayDistance);
-            
-            if (!groundInfo.collider)
-            {
-                // Inverte a direção do inimigo
-                speed = speed * -1;
-
-                // Inverte o sprite do inimigo
-                Vector3 tempScale = transform.localScale;
-                tempScale.x = -tempScale.x;
-                transform.localScale = tempScale;
-            }
 
             Vector2 visionDirection;
             if (transform.localScale.x > 0)
@@ -65,15 +43,13 @@ public class BossScript : MonoBehaviour
                 visionDirection = Vector2.right;
             }
 
-            playerInfo = Physics2D.Raycast(edgeCheck.transform.position, visionDirection, bearVisionArea);
-
-            if (playerInfo.collider && playerInfo.collider.CompareTag("Player"))
+        
+            if (enraged)
             {
                 setSpeed(10);
-                enragedTime = Time.time;
                 animator.SetBool("Enraged", true);
             }
-            else if(Time.time - enragedTime >= 1.5f)
+            else 
             {
                 setSpeed(6);
                 animator.SetBool("Enraged", false);
@@ -113,6 +89,17 @@ public class BossScript : MonoBehaviour
         }
         animator.SetBool("Imune", false);
         setSpeed(6);
+    }
+
+    public void ChangeDirection()
+    {
+        // Inverte a direção do inimigo
+        speed = speed * -1;
+
+        // Inverte o sprite do inimigo
+        Vector3 tempScale = transform.localScale;
+        tempScale.x = -tempScale.x;
+        transform.localScale = tempScale;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
